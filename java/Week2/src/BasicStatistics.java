a64c46d322470ad94c25454723f08df86015760f
HiFiona <90251838+HiFiona@users.noreply.github.com>
2021-09-23 11:59:35 +0800

    Update BasicStatistics.java

--- java/Week2/src/BasicStatistics.java
+++ java/Week2/src/BasicStatistics.java
@@ -1,29 +1,49 @@
+import org.junit.platform.console.shadow.picocli.CommandLine.MaxValuesExceededException;
+
 public class BasicStatistics
 {
     public static int GetSum(int[] n)
     {
         // todo: do your magic here! :)
+    	
+    	for (int i=0;i<n.length;i++) {
+    		
+    	System.out.println(n[i]);
 
-        return 0;
+       
+         }
+         return 0;
     }
 
     public static double GetMean(int[] n)
     {
         // todo: do your magic here! :)
+        double sum = 0;
+        for (int i = 0; i < n.length; i++) {
+            sum += n[i];
+        }
 
-        return 0;
+        return sum / n.length;
     }
 
-    public static int GetMedian(int[] n)
+    public static double GetMedian(int[] n)
     {
         // todo: do your magic here! :)
+        int middle = n.length/2;
+        if (n.length % 2 == 1) {
+            return n[middle];
+        
+        } else {
 
-        return 0;
+            return (n[middle-1] + n [middle])/2.0;
+        
+        }
     }
 
     public static int GetMode(int[] n)
     {
         // todo: do your magic here! :)
+        
 
         return 0;
     }
@@ -31,8 +51,14 @@ public class BasicStatistics
     public static int GetMaxValue(int[] n)
     {
         // todo: do your magic here! :)
+        int MaxValue= n [0];
+        for(int i=1; i< n.length; i++){
+            if(n[i] > MaxValue){
+                MaxValue = n[i];
+            }
 
-        return 0;
+            }
+        return MaxValue;
+        }
     }
 
-}
